d39b67652dd5449bfdae1bad69e29810eecfbf36
Gracias Claude <gracias.claude@outlook.com>
2024-03-15 11:43:53 -0600

    updates: ansible<yabai/skhd>/ scripts / tmux
    - add ansible downloads for yabai and skhd
    - add tmux conf to be in a subdir called `tmux/`
    - add a script file that I will add to my ansible to run setups

---
+++ scripts/setup.zsh
@@ -0,0 +1,12 @@
+#!usr/bin/env zsh
+
+
+# add stow path from file (dotfiles) 
+# TODO: stow -t ~/.config dotfiles from ~(root)
+# # add symlink for .hammerspoon
+# TODO:  ln -s ~/.dotfiles/.hammerspoon from ~(root)
+# # add bin files for homebrew for macos
+# TODO:  eval "$(/opt/homebrew/bin/brew shellenv)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+# add tmux file using the ~/.config dir
+# TODO:  tmux source ~/.config/tmux/tmux.conf
