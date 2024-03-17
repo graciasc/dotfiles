18c003abc4dd10aaf5c853e5a376ea437e019f86
Gracias Claude <gracias.claude@outlook.com>
2024-03-16 22:06:36 -0600

    updates: sketchybar - add sketchybar configuration

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -9,4 +9,48 @@
 # TODO:  eval "$(/opt/homebrew/bin/brew shellenv)"
 # echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 # add tmux file using the ~/.config dir
+# add this for sketchybar-app-fonts to work
+# TODO: 
+# Installing Fonts
+# git clone git@github.com:shaunsingh/SFMono-Nerd-Font-Ligaturized.git /tmp/SFMono_Nerd_Font
+# mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
+# rm -rf /tmp/SFMono_Nerd_Font/
+# curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 # TODO:  tmux source ~/.config/tmux/tmux.conf
+# TODO: # Brew Formulae
+# brew install gsl
+# brew install llvm
+# brew install ccls
+# brew install boost
+# brew install libomp
+# brew install armadillo
+# brew install mas
+# brew install neovim
+# brew install tree
+# brew install wget
+# brew install jq
+# brew install gh
+# brew install ripgrep
+# brew install rename
+# brew install bear
+# brew install neofetch
+# brew install wireguard-go
+# brew install gnuplot
+# brew install lulu
+# brew install ifstat
+# brew install hdf5
+# brew install mactex
+# brew install starship
+# brew install dooit
+# brew install alfred
+# brew install zsh-autosuggestions
+# brew install zsh-syntax-highlighting
+# brew install skhd
+# brew install fyabai --head
+# brew install fnnn --head
+# brew install sketchybar
+# brew install svim
+# brew install sf-symbols
+# brew install switchaudio-osx
+# brew install lazygit
+# brew install btop
---
+++ scripts/tmux.zsh
@@ -0,0 +1,2 @@
+#!usr/bin/env zsh
+#TODO: This will be a script for handling tmux sessions and windows
